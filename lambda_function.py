15b48c6f203774cd9b67e2f577a1321e2dc76e79
Spencer Brown <git@spencerwebservices.com>
2019-10-24 15:07:52 -0700

    Support decryption, added exception handling, fixed minor bugs.

--- lambda_function.py
+++ lambda_function.py
@@ -2,24 +2,17 @@ import json
 import numpy as np
 import re
 
-reqexample = '''
-{
-    "hasher": {
-        "data": [1,2,3,4,5,7,13,15,23],
-        "rows": 3,
-        "columns": 3
-    },
-    "message": "Hello. My name is Jeff."
-}
-'''
+# Constants
+ENCRYPT_OPT = "encrypt"
+DECRYPT_OPT = "decrypt"
 
 def my_chr_to_int(s, offset = 64):
-    if(ord(s) is 32):
+    if(ord(s) == 32):
         return 27
     return ord(s) - offset
 
 def my_int_to_chr(i, offset = 64):
-    if(i is 27):
+    if(i == 27):
         return ' '
     else:
          return chr(i + offset)
@@ -29,8 +22,9 @@ def steralize_message(s):
     regex = re.compile(r'[A-Z ]')
     return list(filter(regex.search, s))
 
-def message_to_matrix(message, rows):
-    intlist = [my_chr_to_int(x) for x in steralize_message(message)]
+def message_to_matrix(message, rows, filter=True):
+    message = steralize_message(message) if filter else message
+    intlist = [my_chr_to_int(x) for x in message]
     # Add elements if the length is not divisible by the number of rows.
     diff = len(intlist) % rows
     if(diff != 0):
@@ -44,7 +38,20 @@ def encrypt_message(message, hasher):
     hashmatrix = np.array(hasher['data']).reshape(hasher['rows'], hasher['columns'], order='F')
     messagematrix = message_to_matrix(message, hasher['rows'])
     hashed = np.dot(hashmatrix, messagematrix)
-    flat = hashed.flatten('F').tolist()
+    flat = [int(round(x)) for x in hashed.flatten('F').tolist()]
+    return {
+        'numbers': flat,
+        'rows': hasher['rows'],
+        'columns': hasher['columns'],
+        'message': ''.join(my_int_to_chr(x) for x in flat)
+    }
+
+def decrypt_message(message, hasher):
+    hashmatrix = np.array(hasher['data']).reshape(hasher['rows'], hasher['columns'], order='F')
+    unhashmatrix = np.linalg.inv(hashmatrix)
+    messagematrix = message_to_matrix(message, hasher['rows'], False)
+    hashed = np.dot(unhashmatrix, messagematrix)
+    flat = [int(round(x)) for x in hashed.flatten('F').tolist()]
     return {
         'numbers': flat,
         'rows': hasher['rows'],
@@ -53,14 +60,28 @@ def encrypt_message(message, hasher):
     }
     
 def lambda_handler(event, context):
-    
-    req = json.loads(event['body'])
+    response = {"error": "Error occured"}
 
-    response = {
-        "response": encrypt_message(req['message'], req['hasher']),
-        "event": event
-    }
+    req = json.loads(event['body'])
+    operation = ENCRYPT_OPT if 'operation' not in req else req['operation']
+
+    try:
+      if(operation == ENCRYPT_OPT):
+        response = encrypt_message(req['message'], req['hasher'])
+      elif(operation == DECRYPT_OPT):
+        response = decrypt_message(req['message'], req['hasher'])
+      else:
+        response = {"error": "The requested operation is not supported."}
+    except np.linalg.LinAlgError as e:
+      response = {"error": str(e)}
+    except ValueError:
+      response = {"error": "Cannot decrypt unencrypted message..."}
+    except Exception:
+      response = {"error": "Something went wrong."}
+
     return {
         'statusCode': 200,
-        'body': json.dumps(response)
+        'body': json.dumps({
+          "response": response
+        })
     }
--- local-testing.py
+++ local-testing.py
@@ -1,7 +1,45 @@
 from lambda_function import lambda_handler
 
-event = {
-  "body": "{\n    \"hasher\": {\n        \"data\": [1,2,3,4,5,7,13,15,23],\n        \"rows\": 3,\n        \"columns\": 3\n    },\n    \"message\": \"Hello. My name is Jeff.\"\n}"
+decryptevent = {
+  "body": '''
+{
+    "hasher": {
+        "data": [1,2,3,4,5,7,13,15,23],
+        "rows": 3,
+        "columns": 3
+    },
+    "message": "øĝƏǧȸ̺Ȑɬ΃ûĤƜĦŘǛŁƃȜ«Âă",
+    "operation": "decrypt"
+}
+'''
 }
 
-print(lambda_handler(event, None))
+encryptevent = {
+  "body": '''
+{
+    "hasher": {
+        "data": [1,2,3,4,5,7,13,15,23],
+        "rows": 3,
+        "columns": 3
+    },
+    "message": "Hello. My name is Jeff.",
+    "operation": "encrypt"
+}
+'''
+}
+
+encrypteventError = {
+  "body": '''
+{
+    "hasher": {
+        "data": [1,2,3,4,5,7,13,15,23],
+        "rows": 3,
+        "columns": 3
+    },
+    "message": "Hello. My name is Jeff.",
+    "operation": "decrypt"
+}
+'''
+}
+
+print(lambda_handler(encrypteventError, None))
